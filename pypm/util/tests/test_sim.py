19596d59eda46c69a040d5a5ee0713d2aa46b279
wehart <whart222@gmail.com>
2021-07-09 21:16:47 -0600

    Adding more tests

--- pypm/util/tests/test_sim.py
+++ pypm/util/tests/test_sim.py
@@ -281,4 +281,74 @@ activities:
                     'a3':[0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0],
                     'a4':[0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0] }
 
+def test_ex6():
+    """
+        a1 -> a2 -> a3
+        a1 -> a4 -> a3
+
+    Activity a4 has a delay starting
+    """
+    data="""
+resources:
+- (Unknown)
+- rA
+- rB
+- rC
+
+activities:
+
+- name: a1
+  max_delay: 4
+  dependencies:
+  duration:
+    max_hours: 4
+    min_hours: 4
+  resources:
+  - rB
+  - rC
+
+- name: a2
+  dependencies:
+  - a1
+  duration:
+    max_hours: 5
+    min_hours: 5
+  resources:
+    rA
+
+- name: a3
+  dependencies:
+  - a2
+  - a4
+  duration:
+    max_hours: 3
+    min_hours: 3
+  resources:
+    rA
+
+- name: a4
+  max_delay: 5
+  dependencies:
+  - a1
+  duration:
+    max_hours: 3
+    min_hours: 3
+  resources:
+    rA
+"""
+    pm = load_data(data=data)
+
+    assert len(pm) == 4
+
+    data = []
+    sim = Simulator(pm=pm, data=data)
+    sim.run(10)
+    #assert data == [(0, 'a1'), (1, 'a1'), (2, 'a1'), (3, 'a1'), (4, 'a2'), (4, 'a4'), (5, 'a2'), (5, 'a4'), (6, 'a2'), (6, 'a4'), (7, 'a2'), (8, 'a2'), (9, 'a3'), (10, 'a3'), (11, 'a3')]
+
+    obs = sim.organize_observations(data, 20)
+    assert obs == { 'a1':[0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0],
+                    'a2':[0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0],
+                    'a3':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0],
+                    'a4':[0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0] }
+
 
