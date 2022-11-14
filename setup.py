c631a8ae4e63cd035fef1f7d5af0d77104a4552d
vjleung <vjleung@sandia.gov>
2022-11-14 06:12:19 -0700

    upped version number

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ requires = [
 packages = _find_packages('pypm')
 
 setup(name='pypm',
-      version='1.2.0',
+      version='1.2.1',
       url='https://cee-gitlab.sandia.gov/adapd/pypm',
       platforms=["any"],
       description='A python library for process matching',
