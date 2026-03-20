287a685e8874fa2dc65fda3f85c57bdf10eb1cf0
whart222 <whart222@gmail.com>
2026-03-20 08:08:07 -0600

    Random restarts near 1

--- pypm/hmm/estimate_emissions.py
+++ pypm/hmm/estimate_emissions.py
@@ -81,11 +81,11 @@ def estimate_emission_parameters(
         if i == 0:
             # Perturb the initial true_positive values
             for k in emission_params.true_positive:
-                emission_params.true_positive[k] *= random.uniform(0.9, 1.0)
+                emission_params.true_positive[k] *= random.uniform(0.95, 1.0)
         else:
             # Generate random true_positive values
             for k in emission_params.true_positive:
-                emission_params.true_positive[k] = random.random()
+                emission_params.true_positive[k] = random.uniform(0.95, 1.0)
         ans_ = _estimate_emission_parameters_iter(
             observed=observed,
             config=config,
